b6ea20b8f234184f58e2f49c79f06eeb80482719
John Meyer <jack10042@gmail.com>
2025-04-26 18:55:28 -0400

    Add build.rs to pull in parsers

---
+++ libs/parser/build.rs
@@ -0,0 +1,83 @@
+use std::panic::catch_unwind;
+use std::path::PathBuf;
+use std::process::Command;
+use std::{env, fs};
+
+const PARSER_CLONE_ROOT: &str = ".vendor";
+
+struct TreeSitterParser<'a> {
+    name: &'a str,
+    repository: &'a str,
+    commit: &'a str,
+}
+
+const PROJECTS: &[TreeSitterParser] = &[TreeSitterParser {
+    name: "tree-sitter-java",
+    repository: "https://github.com/tree-sitter/tree-sitter-java.git",
+    commit: "a7db5227ec40fcfe94489559d8c9bc7c8181e25a",
+}];
+
+fn main() {
+    let root_cwd = env::current_dir().unwrap();
+    for project in PROJECTS {
+        let project_path: PathBuf = [
+            PARSER_CLONE_ROOT,
+            &format!("{}@{}", &project.name, &project.commit),
+        ]
+        .iter()
+        .collect();
+
+        if !project_path.exists() {
+            fs::create_dir_all(&project_path).expect("Failed to create project directory");
+
+            env::set_current_dir(&project_path).unwrap();
+
+            if process(project).is_none() {
+                fs::remove_dir(&project_path).unwrap();
+                panic!("Failed to build project {}", project.name)
+            }
+
+            env::set_current_dir(&root_cwd).unwrap();
+        }
+    }
+}
+
+fn process(project: &TreeSitterParser) -> Option<()> {
+    clone(project)?;
+    catch_unwind(|| compile(project)).ok()?;
+    Some(())
+}
+
+fn clone(project: &TreeSitterParser) -> Option<()> {
+    run("git", &["init", "-q"])?;
+    run("git", &["remote", "add", "origin", project.repository])?;
+    run(
+        "git",
+        &["fetch", "-q", "--depth", "1", "origin", project.commit],
+    )?;
+    run("git", &["checkout", "-q", "FETCH_HEAD"])?;
+    fs::remove_dir_all(".git").ok()?;
+    Some(())
+}
+
+fn compile(parser: &TreeSitterParser) {
+    let dir = &PathBuf::from("src");
+    cc::Build::new()
+        .include(dir)
+        .files(vec![dir.join("parser.c")])
+        .cpp(false)
+        .warnings(false)
+        .compile(parser.name);
+}
+
+fn run(name: &str, args: &[&str]) -> Option<()> {
+    let mut command = Command::new(name);
+    command.args(args);
+    println!("Running {command:?}");
+    command
+        .status()
+        .inspect_err(|_| println!("Failed to execute {command:?}"))
+        .ok()
+        .filter(|cmd| cmd.success())
+        .and(Some(()))
+}
--- libs/parser/src/load_parser.rs
+++ libs/parser/src/load_parser.rs
@@ -1,80 +1,17 @@
-// Adapted from https://github.com/DataDog/datadog-static-analyzer/blob/0fa5eeed99dc91112b1cbb19209e0b36093ef388/crates/static-analysis-kernel/src/analysis/tree_sitter.rs#L22
-
 #![allow(warnings)]
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 pub enum Language {
-    Csharp,
-    Dockerfile,
-    Elixir,
-    Go,
     Java,
-    JavaScript,
-    Json,
-    Kotlin,
-    Python,
-    Ruby,
-    Rust,
-    Swift,
-    Terraform,
-    TypeScript,
-    Yaml,
-    Starlark,
-    Bash,
-    PHP,
-    Markdown,
-    Apex,
-    R,
-    SQL,
 }
 
 pub fn get_tree_sitter_language(language: &Language) -> tree_sitter::Language {
     unsafe extern "C" {
-        fn tree_sitter_c_sharp() -> tree_sitter::Language;
-        fn tree_sitter_dockerfile() -> tree_sitter::Language;
-        fn tree_sitter_elixir() -> tree_sitter::Language;
-        fn tree_sitter_go() -> tree_sitter::Language;
         fn tree_sitter_java() -> tree_sitter::Language;
-        fn tree_sitter_javascript() -> tree_sitter::Language;
-        fn tree_sitter_json() -> tree_sitter::Language;
-        fn tree_sitter_kotlin() -> tree_sitter::Language;
-        fn tree_sitter_python() -> tree_sitter::Language;
-        fn tree_sitter_ruby() -> tree_sitter::Language;
-        fn tree_sitter_rust() -> tree_sitter::Language;
-        fn tree_sitter_swift() -> tree_sitter::Language;
-        fn tree_sitter_tsx() -> tree_sitter::Language;
-        fn tree_sitter_hcl() -> tree_sitter::Language;
-        fn tree_sitter_yaml() -> tree_sitter::Language;
-        fn tree_sitter_starlark() -> tree_sitter::Language;
-        fn tree_sitter_bash() -> tree_sitter::Language;
-        fn tree_sitter_php() -> tree_sitter::Language;
-        fn tree_sitter_markdown() -> tree_sitter::Language;
-        fn tree_sitter_apex() -> tree_sitter::Language;
-        fn tree_sitter_r() -> tree_sitter::Language;
-        fn tree_sitter_sql() -> tree_sitter::Language;
     }
 
-    match language {
-        Language::Csharp => unsafe { tree_sitter_c_sharp() },
-        Language::Dockerfile => unsafe { tree_sitter_dockerfile() },
-        Language::Go => unsafe { tree_sitter_go() },
-        Language::Elixir => unsafe { tree_sitter_elixir() },
-        Language::Java => unsafe { tree_sitter_java() },
-        Language::JavaScript => unsafe { tree_sitter_javascript() },
-        Language::Kotlin => unsafe { tree_sitter_kotlin() },
-        Language::Json => unsafe { tree_sitter_json() },
-        Language::Python => unsafe { tree_sitter_python() },
-        Language::Ruby => unsafe { tree_sitter_ruby() },
-        Language::Rust => unsafe { tree_sitter_rust() },
-        Language::Swift => unsafe { tree_sitter_swift() },
-        Language::Terraform => unsafe { tree_sitter_hcl() },
-        Language::TypeScript => unsafe { tree_sitter_tsx() },
-        Language::Yaml => unsafe { tree_sitter_yaml() },
-        Language::Starlark => unsafe { tree_sitter_starlark() },
-        Language::Bash => unsafe { tree_sitter_bash() },
-        Language::PHP => unsafe { tree_sitter_php() },
-        Language::Markdown => unsafe { tree_sitter_markdown() },
-        Language::Apex => unsafe { tree_sitter_apex() },
-        Language::R => unsafe { tree_sitter_r() },
-        Language::SQL => unsafe { tree_sitter_sql() },
+    unsafe {
+        match language {
+            Language::Java => tree_sitter_java(),
+        }
     }
 }
